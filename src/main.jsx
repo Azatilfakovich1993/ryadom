4ee6e2b0958a8337f146148e227c92553bafff98
azatilfakovich1993-coder <azatilfakovich1993@gmail.com>
2026-04-27 11:37:43 +0400

    feat: add VK Bridge for VK Mini App support

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/index.css'
+import bridge from '@vkontakte/vk-bridge'
+
+// Инициализация VK Bridge если открыто в VK
+bridge.send('VKWebAppInit').catch(() => {})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
